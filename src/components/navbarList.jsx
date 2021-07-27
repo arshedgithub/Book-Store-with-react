0c5651d579cb08a0e1cd073feb088848276e4840
arshedgithub <arshedahmedmail@gmail.com>
2021-07-27 08:29:26 +0530

    add hashrouter

--- src/components/navbarList.jsx
+++ src/components/navbarList.jsx
@@ -4,28 +4,28 @@ import { NavLink } from "react-router-dom";
 const NavbarList = () => {
   return (
     <ul className="navbar-nav text-center">
-      <li className="nav-item px-2">
+      <li className="nav-item p-2">
         <NavLink className="nav-link" aria-current="page" to="/home">
           Home
         </NavLink>
       </li>
-      <li className="nav-item px-2">
+      <li className="nav-item p-2">
         <NavLink className="nav-link" to="/books">
           Books
         </NavLink>
       </li>
-      <li className="nav-item px-2">
+      <li className="nav-item p-2">
         <NavLink className="nav-link" to="/about">
           About
         </NavLink>
       </li>
-      <li className="nav-item px-2">
+      <li className="nav-item p-2">
         <NavLink className="nav-link" aria-current="page" to="/contact">
           Contact
         </NavLink>
       </li>
 
-      <li className="nav-item px-2">
+      <li className="nav-item p-2 px-4">
         <button className="signBtn btn border border-white text-light">
           sign in
         </button>
