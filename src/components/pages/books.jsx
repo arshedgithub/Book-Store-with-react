d35a3ed22b3bb6995ae35ba1964f38574dcd1836
arshedgithub <arshedahmedmail@gmail.com>
2021-07-30 17:41:29 +0530

    UI/UX in books page

--- src/components/pages/books.jsx
+++ src/components/pages/books.jsx
@@ -6,22 +6,28 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Books = () => {
+  let cartcount = 10;
   return (
     <React.Fragment>
       <div className="bg-dark">
         <NavBar />
       </div>
-      <div className="row search mt-4 mx-5">
+      <div className="text-end m-4 text-dark position-relative">
+        <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+        <Link to="/cart" className="text-body ">
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
+            {cartcount}
+          </span>
+        </Link>
+      </div>
+      <div className="search mx-5">
         <input
-          className="col-sm"
+          className="px-2 p-1 w-75"
           type="search"
           name="search"
           id="search"
           placeholder="search books"
         />
-        <Link to="/cart" className="col-sm text-end text-body">
-          <FontAwesomeIcon icon={faShoppingCart} size="2x" />
-        </Link>
       </div>
       <div className="content m-5">
         <BookCategory category={"Adventures"} />
